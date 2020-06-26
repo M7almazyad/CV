6f4720321e5cfc812ab8ea691a7e671036f78251
M7almazyad <M7almazyad@gmail.com>
2020-06-26 18:20:34 +0300

    Updates

---
+++ precache-manifest.960035e90eff907a17c55731227db18f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4343a1b132ff71bda89fce354ea706e9",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "3be5a6a9e560cf0c59a8",
+    "url": "/CV/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ce7071439bf8c383f508",
+    "url": "/CV/static/css/main.9bbd9e52.chunk.css"
+  },
+  {
+    "revision": "3be5a6a9e560cf0c59a8",
+    "url": "/CV/static/js/2.e122b558.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/CV/static/js/2.e122b558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7071439bf8c383f508",
+    "url": "/CV/static/js/main.43dfc108.chunk.js"
+  },
+  {
+    "revision": "755aee1b3b80539d269f",
+    "url": "/CV/static/js/runtime-main.f41502ca.js"
+  },
+  {
+    "revision": "acc23d443700d1eb80978d56e8faa98f",
+    "url": "/CV/static/media/CoEIA.acc23d44.png"
+  },
+  {
+    "revision": "a18c7bf2812ea42bfe383d92be7d2da6",
+    "url": "/CV/static/media/Crawler.a18c7bf2.png"
+  },
+  {
+    "revision": "90fdd41cfd936f6ec0bf0fa16bfbcac7",
+    "url": "/CV/static/media/Cry.90fdd41c.png"
+  },
+  {
+    "revision": "1f59f4f2950a17e4bd7030ad066f1532",
+    "url": "/CV/static/media/EMC.1f59f4f2.png"
+  },
+  {
+    "revision": "9350b310150dfe72b2da8de7d519cb94",
+    "url": "/CV/static/media/Jahez.9350b310.png"
+  },
+  {
+    "revision": "cd55f93948bfe8294b276e9cc26ae845",
+    "url": "/CV/static/media/Ksu.cd55f939.png"
+  },
+  {
+    "revision": "45f86a5578a02b52464d7d67e8cdecc5",
+    "url": "/CV/static/media/Todo.45f86a55.png"
+  },
+  {
+    "revision": "29558276b2575478c033bfcac648a3d8",
+    "url": "/CV/static/media/amaless.29558276.png"
+  },
+  {
+    "revision": "a624d0fcf554c04f37289995fd2a048b",
+    "url": "/CV/static/media/levo.a624d0fc.png"
+  },
+  {
+    "revision": "c92d8e01680758b7be20cf12bd1ded9b",
+    "url": "/CV/static/media/monshaat.c92d8e01.png"
+  },
+  {
+    "revision": "4cd16705c534bd4f0cab3a7fd4cf4b90",
+    "url": "/CV/static/media/myPic6.4cd16705.png"
+  },
+  {
+    "revision": "4eedd269bb45940fb99e2685d8ff4e3f",
+    "url": "/CV/static/media/sql-1.4eedd269.png"
+  },
+  {
+    "revision": "d7b12993b5122343e4702c1e40c7d1f0",
+    "url": "/CV/static/media/webPic.d7b12993.png"
+  }
+]);
